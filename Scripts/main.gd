b4dd0ea2e9d8bed9789c65274329e11114370164
Armando Landero <ar.landero@duocuc.cl>
2026-09-08 12:32:21 -0300

    Se arreglo problema que no permitia lanzar cancion

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,6 +2,7 @@ extends Control
 
 var current_artist = null
 var current_release = null
+var release_ready = null
 
 @onready var money_label = $TopBar/MoneyLabel
 @onready var income_label = $TopBar/IncomeLabel
@@ -10,7 +11,8 @@ var current_release = null
 func _ready() -> void:
 	$ArtistPanel/HireButton.pressed.connect(hire_artist)
 	$ReleasePanel/ReleaseButton.pressed.connect(create_release)
-	
+	$ReleasePanel/ReleaseButton.disabled = true
+	$ReleasePanel/CreateSongButton.pressed.connect(create_song)
 
 # Al borrar esto las stats dejaran de ser mostradas al usuario.
 @warning_ignore("unused_parameter")
@@ -31,6 +33,7 @@ func hire_artist():
 	}
 	
 	GameManager.artists.append(artist)
+	current_artist = artist
 	
 	$ArtistPanel/ArtistName.text = artist.name
 	
@@ -46,25 +49,49 @@ func hire_artist():
 	print("Hired ", artist.name)
 
 func create_release():
-	if current_artist == null:
-		$EventLog.text = "You need an artist before you can make a release!"
+	if not release_ready:
+		$EventLog.text = "You don't have a finished song!"
 		return
-		
-	var inspiration = randf_range(0.0, 100.0) 
+	
+	GameManager.releases.append(current_release)
+	GameManager.income_per_second += current_release.income_per_second
+
+	release_ready = false
+	$ReleasePanel/ReleaseButton.disabled = true
+	$ReleasePanel/CreateSongButton.disabled = false
+
+func create_song():
+	if current_artist == null:
+		$EventLog.text = "You need an artist before you can create a song!"
+		return
+	
+	if release_ready:
+		$EventLog.text = "You already have a song ready to release!"
+	
+	var inspiration = randf_range(0.0, 100)
 	
 	var quality = (
-		current_artist.creativity * 0.4 + current_artist.technique * 0.3 + 50.0 * 0.2 + inspiration + 0.1
+		current_artist.creativity * 0.4 +
+		current_artist.technique * 0.3 +
+		50.0 * 0.2 +
+		inspiration * 0.1
 	)
+	
+	var income = quality * current_artist.charisma * 0.001
 	
 	current_release = {
 		"name": "First Light",
 		"quality": quality,
-		"artist": current_artist.name
+		"artist": current_artist.name,
+		"income_per_second": income
 	}
 	
-	GameManager.releases.append(current_release)
+	release_ready = true
 	
 	$ReleasePanel/SongName.text = current_release.name
 	$ReleasePanel/QualityLabel.text = "Quality: " + str(round(quality)) + "%"
+	$ReleasePanel/ReleaseButton.disabled = false
+	$ReleasePanel/CreateSongButton.disabled = true
 	
-	$EventLog.text = "Released \"" + current_release.name + "\" by " + current_artist.name + "!"
+	$EventLog.text = "The song is ready for release"
+	
